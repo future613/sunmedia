7d428276965b35c0ad475952f710a171f384fcfd
Your Name <you@example.com>
2020-07-01 21:54:37 +0800

    build

---
+++ precache-manifest.dc38b4ee521f29d205e621aa13167f80.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e48a6a1ee844a3860c7dd53962458052",
+    "url": "/sunmedia/index.html"
+  },
+  {
+    "revision": "1a8aca321cb04a330673",
+    "url": "/sunmedia/static/css/2.d788185a.chunk.css"
+  },
+  {
+    "revision": "0ad9f759b42c3703b61d",
+    "url": "/sunmedia/static/css/main.5d2ef262.chunk.css"
+  },
+  {
+    "revision": "1a8aca321cb04a330673",
+    "url": "/sunmedia/static/js/2.961647b9.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/sunmedia/static/js/2.961647b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ad9f759b42c3703b61d",
+    "url": "/sunmedia/static/js/main.ada77be6.chunk.js"
+  },
+  {
+    "revision": "5dda92c4be7c1e163afd",
+    "url": "/sunmedia/static/js/runtime-main.b0ca235b.js"
+  },
+  {
+    "revision": "64523657d9d79a72132b928d096940dc",
+    "url": "/sunmedia/static/media/img.64523657.png"
+  }
+]);
